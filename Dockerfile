7598f3dab1944cd70b17a5c96010d436f95c4358
Chrysochrome <15140725+Chrysochrome@users.noreply.github.com>
2025-11-21 14:43:02 +0800

    更新 Dockerfile 和 index.js：在 Dockerfile 中添加端口暴露，更新 index.js 以实现内存使用情况的定期记录和 CSV 导出功能

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,6 @@ WORKDIR /app
 RUN ~/.bun/bin/bun install
 
 ENV RUNTIME="bun"
+EXPOSE 3000
+
 CMD ["sh", "-c", "if [ \"$RUNTIME\" = \"node\" ]; then node ./index.js; else ~/.bun/bin/bun ./index.js; fi"]
--- index.js
+++ index.js
@@ -2,8 +2,28 @@ import express from 'express';
 const app = express()
 const port = 3000
 
+const results = [];
+
+setInterval(() => {
+  results.push({
+    ...process.memoryUsage(),
+    timestamp: new Date().toISOString()
+  });
+}, 60 * 1000);
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/memory', (req, res) => {
+  // results to csv
+  const csv = results.map(r => {
+    return `${r.timestamp},${r.rss},${r.heapTotal},${r.heapUsed},${r.external},${r.arrayBuffers}`;
+  }).join('\n');
+  res.setHeader('Content-Type', 'text/csv');
+  // filename
+  res.setHeader('Content-Disposition', `attachment; filename="${process.env.RUNTIME ?? 'local'}_memory_usage.csv"`);
+  res.send('timestamp,rss,heapTotal,heapUsed,external,arrayBuffers\n' + csv);
 })
 
 function startSelfHealthCheck() {
